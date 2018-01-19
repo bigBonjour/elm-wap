9bdb4eee12b197faf270626f6d96957f14ce04a1
yangyufeng <1539558157@qq.com>
2018-01-19 10:42:33 +0800

    yangyufeng
    vue.js

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+	<div id="main">
+		<v-header :seller="seller"></v-header>
+		<div class="tab border-1px">
+			<div class="tab-item ">
+				<router-link to="goods">商品</router-link>
+			</div>
+			<div class="tab-item">
+				<router-link to="ratings">评论</router-link>
+			</div>
+			<div class="tab-item">
+				<router-link to="seller">商家</router-link>
+			</div>
+		</div>
+		<keep-alive>
+			<router-view :seller="seller"></router-view>
+		</keep-alive>	
+	</div>
+</template>
+
+<script type="text/ecmascript-6">
+	import header from "@/components/header/header.vue"
+	import { queryParam } from "@/common/js/until"
+	const ERR_OK = 0;
+	export default {
+		data() {
+			return {
+				seller: {}
+			}
+		},
+		components: {
+			"v-header": header
+		},
+		created() {
+			this.$http.get("/api/seller").then((response) => {
+				response = response.body;
+				if(response.errno === ERR_OK) {
+					this.seller = response.data;
+					let id = queryParam().id;
+					this.$set(this.seller, "id", id);
+				}
+			})
+		}
+
+	}
+</script>
+
+<style scoped>
+	.tab {
+		display: flex;
+		width: 100%;
+		height: 40px;
+		line-height: 40px;
+		position: relative;
+	}
+	
+	.tab::after {
+		display: block;
+		content: "";
+		position: absolute;
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		border-top: 5px solid rgba(7, 17, 27, 0.1);
+	}
+	
+	.tab .tab-item {
+		flex: 1;
+		text-align: center;
+	}
+	
+	.tab .tab-item {
+		border-left: 1px solid rgb(212, 214, 216);
+	}
+	
+	.tab .tab-item:first-child {
+		border-left: none;
+	}
+	
+	.tab .tab-item a {
+		display: block;
+		text-decoration: none;
+		font-size: 14px;
+		color: rgb(77, 85, 93);
+	}
+	
+	.tab .tab-item a.router-link-active {
+		color: #f90;
+	}
+</style>
